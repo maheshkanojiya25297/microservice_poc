1030491d290514a40ab3f4de159cf8b6f4c4b57b
mahesh <maheshkanojiya25297@gmail.com>
2023-07-25 18:22:30 +0530

    api gateway file committed

---
+++ ApiGateway/src/main/java/com/lmplemented/gateway/ApiGateway/model/AuthResponse.java
@@ -0,0 +1,23 @@
+package com.lmplemented.gateway.ApiGateway.model;
+
+import lombok.*;
+
+import java.util.Collection;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String userId;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private long expireAt;
+
+    private Collection<String> authorities;
+}
