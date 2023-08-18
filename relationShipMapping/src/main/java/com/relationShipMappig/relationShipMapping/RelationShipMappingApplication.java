5ce93e7bbb0e10bfd054a09e5a001a81b2eab217
mahesh <maheshkanojiya25297@gmail.com>
2023-08-18 16:17:49 +0530

    Relation Ship mapping implementation

---
+++ relationShipMapping/src/main/java/com/relationShipMappig/relationShipMapping/RelationShipMappingApplication.java
@@ -0,0 +1,13 @@
+package com.relationShipMappig.relationShipMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationShipMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationShipMappingApplication.class, args);
+	}
+
+}
---
+++ relationShipMapping/src/test/java/com/relationShipMappig/relationShipMapping/RelationShipMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relationShipMappig.relationShipMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationShipMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
