85fddfcd30ac01e8a86405605ef1f35cb13c3755
mahesh <maheshkanojiya25297@gmail.com>
2023-09-04 12:45:32 +0530

    Swagger Open Ui integrated with Jwt Authentication Spring security.

--- jwtGeneration/src/main/java/com/jwt/token/generation/config/SecurityConfig.java
+++ jwtGeneration/src/main/java/com/jwt/token/generation/config/SecurityConfig.java
@@ -2,10 +2,18 @@ package com.jwt.token.generation.config;
 
 import com.jwt.token.generation.security.JwtAuthenticationEntryPoint;
 import com.jwt.token.generation.security.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,7 +34,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.disable())
                 .authorizeRequests(
                         auth -> auth.requestMatchers("/home/**").authenticated()
-                                .requestMatchers("auth/login")
+                                .requestMatchers("auth/login", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/*", "home/user")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
@@ -37,4 +45,25 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("mahesh")
+                                .email("mahesh").url("salloszraj@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
 }
---
+++ jwtGeneration/src/main/java/com/jwt/token/generation/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.jwt.token.generation.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(title = "jwt-poc"),
+        servers = {@Server(
+                description = "Swagger Url",
+                url = "http://localhost:8081/"
+        )}
+)
+public class SwaggerConfig {
+
+}
