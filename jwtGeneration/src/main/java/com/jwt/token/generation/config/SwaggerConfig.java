e3b49dcab559438ad76aa02d319191a643ef20f7
mahesh <maheshkanojiya25297@gmail.com>
2023-09-04 13:08:51 +0530

    Bearer Authorize component added.

--- jwtGeneration/src/main/java/com/jwt/token/generation/config/SwaggerConfig.java
+++ jwtGeneration/src/main/java/com/jwt/token/generation/config/SwaggerConfig.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-        info = @Info(title = "jwt-poc"),
+        info = @Info(title = "Jwt-Token_Authentication"),
         servers = {@Server(
                 description = "Swagger Url",
                 url = "http://localhost:8081/"
