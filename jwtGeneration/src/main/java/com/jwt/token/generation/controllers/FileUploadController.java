88599cd82508a92d160ea5424af5a88b27b674f2
mahesh <maheshkanojiya25297@gmail.com>
2023-09-08 17:33:38 +0530

    multiple upload method added.

--- jwtGeneration/src/main/java/com/jwt/token/generation/controllers/FileUploadController.java
+++ jwtGeneration/src/main/java/com/jwt/token/generation/controllers/FileUploadController.java
@@ -5,7 +5,9 @@ import com.jwt.token.generation.model.Response;
 import com.jwt.token.generation.service.FileStorageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -21,15 +27,31 @@ public class FileUploadController {
     @Autowired
     private FileStorageService fileStorageService;
 
-    @PostMapping(value = "/uploadFile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Response uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info("single file uploaded : {}" );
         String fileName = fileStorageService.storeFile(file);
-        log.info("fileName: " +fileName);
+        log.info("fileName: " + fileName);
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/downloadFile/")
                 .path(fileName)
                 .toUriString();
-        log.info("fileDownloadUri: " +fileDownloadUri);
+        log.info("fileDownloadUri: " + fileDownloadUri);
         return new Response(fileName, fileDownloadUri, file.getContentType(), file.getSize());
- }
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<Response> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        log.info("multiple file uploaded: {} " );
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> {
+                    try {
+                          return uploadFile(file);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
 }
