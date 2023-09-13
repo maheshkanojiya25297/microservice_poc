eaf28e25f3bf3c28314ef71aabfba5bbacfa5619
mahesh <maheshkanojiya25297@gmail.com>
2023-09-13 15:38:03 +0530

    Read Text file added Twilio Sms Push Notification Service.

--- twilioPushNotification/src/main/java/com/twilio/twilioPushNotification/Controller.java
+++ twilioPushNotification/src/main/java/com/twilio/twilioPushNotification/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("api/smsOtp")
+@RequestMapping("smsOtp")
 public class Controller {
 
     private final Service service;
