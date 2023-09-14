b103e94b6c80ee80ad472eb2a329c2daafe89e62
mahesh <maheshkanojiya25297@gmail.com>
2023-09-14 14:53:45 +0530

    AutoReloading Devtools Feature added In existing Project

---
+++ ExelFileUploadApachePoI/src/main/java/ExelFileUploadApachePoI/ExelFileUploadApachePoIApplication.java
@@ -0,0 +1,15 @@
+package ExelFileUploadApachePoI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExelFileUploadApachePoIApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExelFileUploadApachePoIApplication.class, args);
+		System.out.println("Application started ):");
+	}
+
+}
---
+++ ExelFileUploadApachePoI/src/test/java/ExelFileUploadApachePoI/ExelFileUploadApachePoIApplicationTests.java
@@ -0,0 +1,13 @@
+package ExelFileUploadApachePoI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExelFileUploadApachePoIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
