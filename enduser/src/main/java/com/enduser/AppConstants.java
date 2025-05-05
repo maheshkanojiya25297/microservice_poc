f07ef36aa6d70d528ac21205d2bbc82a9510652a
maheshkanojiya25297 <mahesh.kanojiya@neosoftmail.com>
2025-05-05 17:10:02 +0530

    POC project implemented for kafka usage where deliveryboy is producer and enduser is consumer

---
+++ enduser/src/main/java/com/enduser/AppConstants.java
@@ -0,0 +1,6 @@
+package com.enduser;
+
+public class AppConstants {
+    public static final String LOCATION_UPDATE_TOPIC = "location_update_topic"; // Match the topic name from the CLI
+    public static final String GROUP_ID = "group-1";
+}
---
+++ enduser/src/main/java/com/enduser/KafkaConfig.java
@@ -0,0 +1,19 @@
+package com.enduser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+@Configuration
+public class KafkaConfig {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value) {
+        //logger.info("Message Consumed: {}", value);
+        System.out.println("enduser value: " + value);
+    }
+}
